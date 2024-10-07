9195172f71ab3ca81854578f521f00d25e546afc
Plugin <tja414312570@gmail.com>
2024-10-07 22:33:28 +0800

    111

--- electron-vite-template/plugins/proxy/src/hello.ts
+++ electron-vite-template/plugins/proxy/src/hello.ts
@@ -1,4 +1,11 @@
+import { Bridge } from "../../../src/type/bridge";
+
 export function sayHello(name: string): void {
     console.log(`Hello, ${name}!`);
   }
   
+class Test implements Bridge{
+  onResponse(body: any): void {
+    throw new Error("Method not implemented.");
+  }
+}
